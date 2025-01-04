de0a7afd4762e8c1f5494a39acec8ed2cf298c7e
goutam <goutamr362@gmail.com>
2025-01-04 22:21:59 +0600

    edge template dynamic

---
+++ app/data.php
@@ -0,0 +1,174 @@
+<?php
+
+// about content 
+$about = [
+  "title"     => "About Us",
+  "desc"      => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorem eum consequuntur ipsam repellat dolor soluta aliquid laborum, eius odit consectetur vel quasi in quidem, eveniet ab est corporis tempore.',
+  "button"    => [
+       "btn_title" => "Apply Now",
+       "btn_link"  => "#",
+       "bg_color"  => "red"
+  ],
+  "photo"    => "https://i.ibb.co.com/MRNrrsF/about-img.png"
+
+];
+
+// slider content 
+$slider = [
+  [
+    "title"   => "We love Bangladesh",
+    "desc"    => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "photo"   => "https://i.ibb.co.com/MRNrrsF/about-img.png",
+    "button"  => [
+        "btn_1"  => [
+          "title" => "Read More Now",
+          "link"  => "#",
+        ],
+        "btn_2"  => [
+          "title" => "Contact Us",
+          "link"  => "#",
+        ],
+    ],
+  ],
+  [
+    "title"   => "We love Laravel",
+    "desc"    => "Lorem ipsum dolor sit amet consectetur adipisicing elit. aliquid laborum, eius odit consectetur ",
+    "photo"   => "https://i.ibb.co.com/3FCyfxZ/istockphoto-1368953777-612x612.jpg",
+    "button"  => [
+        "btn_1"  => [
+          "title" => "Add To Cart",
+          "link"  => "#",
+        ],
+        "btn_2"  => [
+          "title" => "Our Servives",
+          "link"  => "#",
+        ],
+    ],
+  ],
+  [
+    "title"   => "We love MERN STACK",
+    "desc"    => "Lorem ipsum dolor sit amet consectetur adipisicing elit. eveniet ab est corporis tempore.",
+    "photo"   => "https://i.ibb.co.com/4K5KYWR/model-portfolio-vector-illustration-with-modeling-agency-manager-and-photographer-take-photos-of-mod.jpg",
+    "button"  => [
+        "btn_1"  => [
+          "title" => "Buy Now",
+          "link"  => "#",
+        ],
+        "btn_2"  => [
+          "title" => "Explore Menu",
+          "link"  => "#",
+        ],
+    ],
+  ],
+];
+
+// furniture content
+$furniture = [
+   "title"      => "Our Favourite Furniture",
+   "sub_title"  => "which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't an updated Product",
+   "products"   => [
+    [
+      "name"        => "brown Chair Design",
+      "reg_price"   => "100",
+      "sale_price"  => "80",
+      "photo"       => "https://i.ibb.co.com/qB7tx89/f1.png",
+      "button"      => [
+                    "title" => "Add To Cart",
+                    "link"  => "#"
+                   ]
+    ],
+    [
+      "name"        => "Double Bed Design",
+      "reg_price"   => "780",
+      "sale_price"  => "680",
+      "photo"       => "https://i.ibb.co.com/5LjD5T6/f2.png",
+      "button"      => [
+                    "title" => "Add To Cart",
+                    "link"  => "#"
+                   ]
+    ],
+    [
+      "name"        => "House Chair Design",
+      "reg_price"   => "550",
+      "sale_price"  => "440",
+      "photo"       => "https://i.ibb.co.com/DDf38Lr/f3.png",
+      "button"      => [
+                    "title" => "Add To Cart",
+                    "link"  => "#"
+                   ]
+    ],
+    [
+      "name"        => "brown Table Design",
+      "reg_price"   => "300",
+      "sale_price"  => "280",
+      "photo"       => "https://i.ibb.co.com/m8L71qm/f4.png",
+      "button"      => [
+                    "title" => "Add To Cart",
+                    "link"  => "#"
+                   ]
+    ],
+    [
+      "name"        => "Blue Chair Design",
+      "reg_price"   => "200",
+      "sale_price"  => "180",
+      "photo"       => "https://i.ibb.co.com/NmxtxYF/f5.png",
+      "button"      => [
+                    "title" => "Add To Cart",
+                    "link"  => "#"
+                   ]
+    ],
+    [
+      "name"        => "Brown Table Design",
+      "reg_price"   => "150",
+      "sale_price"  => "120",
+      "photo"       => "https://i.ibb.co.com/fY2mhkp/f6.png",
+      "button"      => [
+                    "title" => "Add To Cart",
+                    "link"  => "#"
+                   ]
+    ],
+   ]
+]; 
+
+// blog content 
+$blog = [
+  "title" => "our Latest Blog",
+  "blogs" => [
+    [
+      "title"  => "Look even slightly believable. If you are",
+      "desc"   => "alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+      "button" => [
+        "title" => "Read More",
+        "link"  => "#"
+
+      ],
+      "photo"   => "https://i.ibb.co.com/RzhTvCt/b1.jpg"
+
+    ],
+    [
+      "title"  => "Anything embarrassing hidden in the middle",
+      "desc"   => "You can create a new album with the content just uploaded. You must create an account or sign in to save this content into your account.",
+      "button" => [
+        "title" => "Read More",
+        "link"  => "#"
+
+      ],
+      "photo"   => "https://i.ibb.co.com/RT62BPv/b2.jpg"
+
+    ],
+    [
+      "title"  => "Molestias magni natus dolores odio commodi.",
+      "desc"   => "alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+      "button" => [
+        "title" => "Read More",
+        "link"  => "#"
+
+      ],
+      "photo"   => "https://i.ibb.co.com/YLgX6nz/b3.jpg"
+
+    ],
+
+  ]
+]
+
+?>
